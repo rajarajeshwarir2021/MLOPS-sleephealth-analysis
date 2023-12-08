19083a13ee3bb95e3f622a27a341718ffeb90510
rajar <rajarajeshwarir2021@gmail.com>
2023-12-08 12:01:18 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an MLOPS based project for predicting the sleep health of a person based on their habits and various other factors.',
+    author='raji',
+    license='MIT',
+)
